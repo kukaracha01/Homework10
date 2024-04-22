80754cc817866cda4baaaa731b5d58a72f9d64a6
mu <semkadarov@gmail.com>
2024-04-22 23:20:54 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println("Task1");
+
     }
 }
